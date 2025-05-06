c774166b92a35d27f48813dd5a07c794586b241a
Kross1de <awesome.hubygu@yandex.ru>
2025-05-06 22:33:05 +0600

    Update cursor

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,3 @@
-/*
-Commented code at https://wiki.osdev.org/Bare_Bones#Writing_a_kernel_in_C
-*/
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -13,7 +10,8 @@ Commented code at https://wiki.osdev.org/Bare_Bones#Writing_a_kernel_in_C
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
-enum vga_color {
+enum vga_color
+{
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
 	VGA_COLOR_GREEN = 2,
@@ -32,25 +30,34 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 };
 
+static inline void outb(uint16_t port, uint8_t val)
+{
+	__asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
 static inline uint8_t vga_entry_color(
-	enum vga_color fg, enum vga_color bg
-){
+	enum vga_color fg, enum vga_color bg)
+{
 	return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
-	return (uint16_t) uc | (uint16_t) color << 8;
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
+{
+	return (uint16_t)uc | (uint16_t)color << 8;
 }
 
-size_t strlen(const char* str) {
+size_t strlen(const char *str)
+{
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
 
-void strcpy(char* dst, const char* src) {
-	while(*dst++ = *src++);
+void strcpy(char *dst, const char *src)
+{
+	while (*dst++ = *src++)
+		;
 }
 
 static const size_t VGA_WIDTH = 80;
@@ -59,62 +66,85 @@ static const size_t VGA_HEIGHT = 25;
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer;
+uint16_t *terminal_buffer;
 
-void terminal_initialize(void) {
+void update_cursor(void)
+{
+	uint16_t pos = terminal_row * VGA_WIDTH + terminal_column;
+	outb(0x3d4, 0x0e);
+	outb(0x3d5, (pos >> 8) & 0xff);
+	outb(0x3d4, 0x0f);
+	outb(0x3d5, pos & 0xff);
+}
+
+void terminal_initialize(void)
+{
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(
-		VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK
-	);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH +x;
+		VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t *)0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++)
+	{
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+		{
+			const size_t index = y * VGA_WIDTH + x;
 			terminal_buffer[index] = vga_entry(
-				' ', terminal_color
-			);
+				' ', terminal_color);
 		}
 	}
+	update_cursor();
 }
 
-void terminal_setcolor(uint8_t color) {
+void terminal_setcolor(uint8_t color)
+{
 	terminal_color = color;
 }
 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+{
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-void newline() {
+void newline()
+{
 	terminal_column = 0;
 	if (++terminal_row == VGA_HEIGHT)
 		terminal_row = 0;
+	update_cursor();
 }
 
-void terminal_putchar(char c) {
-	if (c == '\n') {
+void terminal_putchar(char c)
+{
+	if (c == '\n')
+	{
 		newline();
 	}
-	else {
+	else
+	{
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-		if (++terminal_column == VGA_WIDTH) {
+		if (++terminal_column == VGA_WIDTH)
+		{
 			newline();
 		}
 	}
+	update_cursor();
 }
 
-void terminal_write(const char* data, size_t size) {
+void terminal_write(const char *data, size_t size)
+{
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
 
-void terminal_writestring(const char* data) {
+void terminal_writestring(const char *data)
+{
 	terminal_write(data, strlen(data));
 }
 
-void kernel_main(void) {
+void kernel_main(void)
+{
 	terminal_initialize();
 	terminal_writestring("wiki.osdev.org\n");
 	terminal_writestring("OSDev started");
