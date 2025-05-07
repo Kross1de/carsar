19d158ae066cbda12486493b009b86369ca8d276
Kross1de <awesome.hubygu@yandex.ru>
2025-05-07 14:47:17 +0600

    Rewrite moment (x86_64)

---
+++ kernel/arch/x86_64/main.c
@@ -0,0 +1,16 @@
+#include <arch/x86_64/gdt.h>
+#include <misc/printf.h>
+#include <video/vga.h>
+#include <sys/version.h>
+
+void kmain(void *mboot_info) {
+    vga_clear();
+    vga_enable_cursor();
+
+    printf("%s %d.%d.%d %s %s %s\n",
+        __kernel_name, __kernel_version_major, __kernel_version_minor,
+        __kernel_version_patch, __kernel_build_date, __kernel_build_time,
+        __kernel_arch);
+
+    gdt_install();
+}
